0e48a757bceba9191c3418d72948d7bd03a784bc
stinta <kyrbynyt@gmail.com>
2025-06-04 01:05:49 +0300

    arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+use colored::Colorize;
+
 fn main() {
-    println!("Hello, world!");
+    let config = match Config::from_args(std::env::args()) {
+        Some(c) => c,
+        None => return,
+    };
+    let input = match std::fs::read_to_string(&config.input_name) {
+        Ok(i) => i,
+        Err(e) => {
+            eprintln!(
+                "[{}]: Failed to read {input_name}: {e}",
+                "Error".red(),
+                input_name = config.input_name,
+            );
+            usage(&config.program_name);
+            return;
+        }
+    };
+    println!("{input}");
+}
+
+struct Config {
+    program_name: String,
+    input_name: String,
+}
+
+impl Config {
+    fn from_args(mut args: std::env::Args) -> Option<Self> {
+        let program_name = args.next().expect("No standard compliance?");
+        let input_name = match args.next() {
+            Some(n) => n,
+            None => {
+                eprintln!("[{}]: No .gp file provided!", "Error".red());
+                usage(&program_name);
+                return None;
+            }
+        };
+        Some(Self {
+            input_name,
+            program_name,
+        })
+    }
+}
+
+fn usage(prog_name: &str) {
+    eprintln!("  [{}]: Usage:", "Info".blue());
+    eprintln!("  [{}]: ./{prog_name} <input.gp>", "Info".blue());
 }
