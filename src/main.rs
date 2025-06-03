1e59b6c211f0402193aeee5a528d6f06745955ca
stinta <kyrbynyt@gmail.com>
2025-06-04 01:32:23 +0300

    begin initial sketch

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
         Some(c) => c,
         None => return,
     };
-    let input = match std::fs::read_to_string(&config.input_name) {
+    let input: Vec<_> = match std::fs::read_to_string(&config.input_name) {
         Ok(i) => i,
         Err(e) => {
             eprintln!(
@@ -17,8 +17,79 @@ fn main() {
             usage(&config.program_name);
             return;
         }
-    };
-    println!("{input}");
+    }
+    .chars()
+    .collect();
+
+    for t in Lexer::new(&input) {
+        dbg!(t);
+    }
+}
+
+#[derive(Debug)]
+pub struct Lexer<'a> {
+    input: &'a [char],
+    offset: usize,
+    line_beginning: usize,
+}
+
+#[derive(Debug, Clone)]
+pub enum Error {
+    UnexpectedChar(char)
+}
+#[derive(Debug, Clone)]
+pub enum Token {
+    Number(u64)
+}
+
+#[derive(Debug, Clone)]
+pub struct Spanned<T> {
+    span: std::ops::Range<usize>,
+    v: T,
+}
+
+pub type Span = std::ops::Range<usize>;
+
+impl<'a> Lexer<'a> {
+    pub fn new(src: &'a [char]) -> Self {
+        Self {
+            input: src,
+            offset: 0,
+            line_beginning: 0,
+        }
+    }
+
+    fn skip_ws(&mut self) {
+        while !self.finished() && self.input[self.offset].is_whitespace() {
+            match self.input[self.offset] {
+                ' ' | '\t' => self.offset += 1,
+                '\n' => {
+                    self.offset += 1;
+                    self.line_beginning += self.offset;
+                }
+                _ => unreachable!(),
+            }
+        }
+    }
+    fn finished(&self) -> bool {
+        self.offset >= self.input.len()
+    }
+}
+
+impl<'a> Iterator for Lexer<'a> {
+    type Item = Result<Spanned<Token>, Spanned<Error>>;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.skip_ws();
+        if self.finished() {
+            return None;
+        }
+        match self.input[self.offset] {
+            c if c.is_ascii_digit() => {
+                todo!()
+            }
+            _ => 
+        }
+    }
 }
 
 fn usage(prog_name: &str) {
