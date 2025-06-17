11fe81c0d9c49ff0c94039afa272ea262a80c625
stinta <kyrbynyt@gmail.com>
2025-06-17 15:50:15 +0300

    lets make our own IR

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod parser;
 mod type_check;
 
 use colored::Colorize;
-use std::io::Write;
+use std::{collections::HashMap, io::Write};
 
 fn main() {
     let config = match config::Config::from_args(std::env::args()) {
@@ -58,7 +58,7 @@ fn main() {
 
     let pre_t_check = std::time::Instant::now();
     let errs = type_check::type_check(&program);
-    println!("Type checking took: {:.2?}", pre_t_check.elapsed());
+    println!("[{}]: Type checking took: {:.2?}", "Info".green(), pre_t_check.elapsed());
     for e in &errs {
         eprintln!("{}", e.v);
         display_diagnostic_info(&input, &config.input_name, e);
@@ -67,15 +67,187 @@ fn main() {
         return;
     }
 
-    let pre_cg = std::time::Instant::now();
-    let module = codegen::generate_qbe_module(&program);
-    println!("Code geneneration took: {:.2?}", pre_cg.elapsed());
-    if let Err(e) = compile_qbe_module(module, &config.input_name[0..config.input_name.len() - 3]) {
-        eprintln!("[{}]\n Failed to compile code module: {e}", "Error".red());
+
+    let ir = generate_ir(&program);
+    display_ir(&ir);
+
+
+    // let pre_cg = std::time::Instant::now();
+    // let module = codegen::generate_qbe_module(&program);
+    // println!("[{}]: Code geneneration took: {:.2?}", "Info".green(), pre_cg.elapsed());
+    // if let Err(e) = compile_qbe_module(module, &config.input_name[0..config.input_name.len() - 3]) {
+    //     eprintln!("[{}]\n Failed to compile code module: {e}", "Error".red());
+    // }
+}
+
+
+
+#[derive(Debug)]
+enum Value {
+    Const(u64)
+}
+
+type ValueIndex = usize;
+type LabelIndex = usize;
+
+#[derive(Debug)]
+enum Instr {
+    Assign {
+        index: ValueIndex,
+        v: Value
+    },
+    Reassign {
+        index: ValueIndex,
+        new_value_index: ValueIndex,
+    },
+    BinaryOp {
+        op: lexer::Operator,
+        l: ValueIndex,
+        r: ValueIndex,
+        into: ValueIndex
+    },
+    UnaryOp {
+        op: lexer::Operator,
+        r: ValueIndex,
+        into: ValueIndex
+    },
+    Return {
+        index: ValueIndex,
+    },
+    Label(LabelIndex),
+    Jump(LabelIndex),
+    JumpNotZero { 
+        index: ValueIndex, 
+        to: LabelIndex,
+        otherwise: LabelIndex
+    },
+}
+
+fn generate_ir(module: &[Spanned<parser::Statement>]) -> Vec<Instr> {
+    let mut ir = vec![];
+    let mut vars: HashMap<String, ValueIndex> = HashMap::new();
+
+    let mut t_count = 0;
+    let mut l_count = 0;
+    
+    for s in module {
+        generate_statement(&mut ir, &mut vars, &s.v, &mut t_count, &mut l_count);
+    }
+
+    ir
+}
+
+fn alloc_new(t: &mut usize) -> usize {
+    *t += 1;
+    *t - 1
+}
+
+fn generate_statement(ir: &mut Vec<Instr>, vars: &mut HashMap<String, ValueIndex>, s: &parser::Statement, t_count: &mut ValueIndex, l_count: &mut LabelIndex) {
+    match s {
+        parser::Statement::Return(v) => {
+            let e = generate_expr(ir, vars, &v.v, t_count);
+            ir.push(Instr::Return { index: e });
+        }
+        parser::Statement::VarAssign { name, t: _, expr } => {
+            let e = generate_expr(ir, vars, &expr.v, t_count);
+            vars.insert(name.to_string(), e);
+        }
+        parser::Statement::If { cond, body } => {
+            let e = generate_expr(ir, vars, &cond.v, t_count);
+            let body_idx = alloc_new(l_count);
+            let over = alloc_new(l_count);
+            ir.push(Instr::JumpNotZero {
+                to: body_idx,
+                otherwise: over,
+                index: e,
+            });
+            ir.push(Instr::Label(body_idx));
+            for i in body {
+                generate_statement(ir, vars, &i.v, t_count, l_count);
+            }
+            ir.push(Instr::Label(over));
+        }
+        parser::Statement::VarReassign { name, expr } => {
+            let index = *vars.get(name).unwrap();
+            let new = generate_expr(ir, vars, &expr.v, t_count);
+            ir.push(Instr::Reassign { index, new_value_index: new });
+        }
+        parser::Statement::While { cond, body } => {
+            let header_idx = alloc_new(l_count);
+            let body_idx = alloc_new(l_count);
+            let over_idx = alloc_new(l_count);
+            ir.push(Instr::Label(header_idx));
+            let e = generate_expr(ir, vars, &cond.v, t_count);
+            ir.push(Instr::JumpNotZero { index: e, to: body_idx, otherwise: over_idx });
+            ir.push(Instr::Label(body_idx));
+            for b in body {
+                generate_statement(ir, vars, &b.v, t_count, l_count);
+            }
+            ir.push(Instr::Jump(header_idx));
+            ir.push(Instr::Label(over_idx));
+        }
+    }
+}
+
+fn generate_expr(ir: &mut Vec<Instr>, vars: &mut HashMap<String, ValueIndex>, e: &parser::Expression, t_count: &mut usize) -> ValueIndex {
+    match e {
+        parser::Expression::Integer(i) => {
+            let place = alloc_new(t_count);
+            ir.push(Instr::Assign { index: place, v: Value::Const(*i) });
+            place
+        }
+        parser::Expression::Bool(b) => {
+            let place = alloc_new(t_count);
+            ir.push(Instr::Assign { index: place, v: Value::Const(*b as u64) });
+            place
+        }
+        parser::Expression::Identifier(name) => {
+            *vars.get(name).unwrap()
+        }
+        parser::Expression::Binary { left, op, right } => {
+            let left = generate_expr(ir, vars, &left.v, t_count);
+            let right = generate_expr(ir, vars, &right.v, t_count);
+            let place = alloc_new(t_count);
+            ir.push(Instr::BinaryOp { op: *op, l: left, r: right, into: place });
+            place
+        }
+        parser::Expression::Unary { op, right } => {
+            let right = generate_expr(ir, vars, &right.v, t_count);
+            let place = alloc_new(t_count);
+            ir.push(Instr::UnaryOp { op: *op, r: right, into: place } );
+            place
+        }
+    }
+}
+
+fn display_ir(ir: &[Instr]) {
+    for i in ir {
+        match i {
+            Instr::Assign { index, v } => {
+                println!("Temp[{index}] = {v:?}")
+            }
+            Instr::BinaryOp { op, l, r, into } => {
+                println!("Temp[{into}] = Temp[{l}] {op:?} Temp[{r}]")
+            }
+            Instr::UnaryOp { op, r, into } => {
+                println!("Temp[{into}] = {op:?} Temp[{r}]")
+            }
+            Instr::Return { index } => {
+                println!("Return Temp[{index}]")
+            }
+            Instr::Label(l) => {
+                println!("Label[{l}]");
+            }
+            Instr::JumpNotZero { index, to, otherwise} => {
+                println!("JumpNotZero Label[{to}] else Label[{otherwise}] Temp[{index}]");
+            }
+            r => todo!("{r:?}")
+        }
     }
 }
 
 fn compile_qbe_module(module: qbe::Module, base_name: &str) -> std::io::Result<()> {
+    let pre = std::time::Instant::now();
     let ssa_name = format!("{}.ssa", &base_name);
     let s_name = format!("{}.s", &base_name);
     let mut file_ssa = std::io::BufWriter::new(
@@ -99,6 +271,7 @@ fn compile_qbe_module(module: qbe::Module, base_name: &str) -> std::io::Result<(
         .arg(base_name)
         .spawn()?
         .wait()?;
+    println!("[{}]: Compilation took {:.2?}", "Info".green(), pre.elapsed());
     std::process::Command::new("rm")
         .arg(&s_name)
         .arg(&ssa_name)
