7dc443df94c26f959fff462ea7e37c5621815ad3
stinta <kyrbynyt@gmail.com>
2025-06-20 19:31:02 +0300

    quick save THIS DOES NOT BUILD

--- src/codegen/qbe.rs
+++ src/codegen/qbe.rs
@@ -1,107 +1,11 @@
 use crate::{ir::{self, LabelIndex, ValueIndex}, lexer};
 use qbe::{Module, Function, Linkage, Type, Value};
+use std::collections::HashSet;
 
 pub fn generate_qbe_module(ir: &[ir::Instr]) -> Module {
     let mut module = Module::new();
     let main = module.add_function(Function::new(Linkage::public(), "main", vec![], Some(Type::Word)));
     main.add_block("entry");
 
-    for i in ir {
-        match i {
-            ir::Instr::Assign { index, v } => {
-                alloc_and_store_value(main, *v, *index);
-            }
-            ir::Instr::Return { value } => {
-                let value = load_value(main, *value);
-                main.add_instr(qbe::Instr::Ret(Some(value)));
-            }
-            ir::Instr::Label(idx) => {
-                add_label(main, *idx);
-            }
-            ir::Instr::JumpNotZero { index, to, otherwise } => {
-                let place = Value::Temporary(format!("value_{index}"));
-                let from = Value::Temporary(format!("ptr_{index}"));
-                main.assign_instr(place.clone(), qbe::Type::Word, qbe::Instr::Load(qbe::Type::Word, from));
-                main.add_instr(qbe::Instr::Jnz(place, format!("l_{to}"), format!("l_{otherwise}")));
-            }
-            ir::Instr::BinaryOp { op, l, r, into } => {
-                let l = load_value(main, *l);
-                let r = load_value(main, *r);
-                let val = Value::Temporary(format!("value_{into}"));
-                let ptr = Value::Temporary(format!("ptr_{into}"));
-                main.assign_instr(ptr.clone(), qbe::Type::Long, qbe::Instr::Alloc8(8));
-                match op {
-                    lexer::Operator::Plus => main.assign_instr(val.clone(), qbe::Type::Word, qbe::Instr::Add(l, r)),
-                    lexer::Operator::Minus => main.assign_instr(val.clone(), qbe::Type::Word, qbe::Instr::Sub(l, r)),
-                    lexer::Operator::Star => main.assign_instr(val.clone(), qbe::Type::Word, qbe::Instr::Mul(l, r)),
-                    lexer::Operator::Slash => main.assign_instr(val.clone(), qbe::Type::Word, qbe::Instr::Div(l, r)),
-                    lexer::Operator::Percent => main.assign_instr(val.clone(), qbe::Type::Word, qbe::Instr::Rem(l, r)),
-                    lexer::Operator::Less => main.assign_instr(val.clone(), qbe::Type::Word, qbe::Instr::Cmp(qbe::Type::Word, qbe::Cmp::Slt, l, r)),
-                    lexer::Operator::More => main.assign_instr(val.clone(), qbe::Type::Word, qbe::Instr::Cmp(qbe::Type::Word, qbe::Cmp::Sgt, l, r)),
-                    lexer::Operator::Not => unreachable!()
-                }
-                main.add_instr(qbe::Instr::Store(qbe::Type::Word, ptr, val));
-            }
-            ir::Instr::UnaryOp { op, r, into } => {
-                let place = Value::Temporary(format!("value_{r}"));
-                let from = Value::Temporary(format!("ptr_{r}"));
-                main.assign_instr(place.clone(), qbe::Type::Word, qbe::Instr::Load(qbe::Type::Word, from));
-                match op {
-                    lexer::Operator::Not => {
-                        let place = alloc_value(main, *into);
-                        let val = Value::Temporary(format!("value_{into}"));
-                        main.assign_instr(val.clone(), qbe::Type::Word, qbe::Instr::Cmp(qbe::Type::Word, qbe::Cmp::Eq, Value::Const(0), place.clone()));
-                        main.add_instr(qbe::Instr::Store(qbe::Type::Word, place, val));
-                    },
-                    lexer::Operator::Minus => {
-                        let place = alloc_value(main, *into);
-                        let val = Value::Temporary(format!("value_{into}"));
-                        main.assign_instr(val.clone(), qbe::Type::Word, qbe::Instr::Sub(Value::Const(0), place.clone()));
-                        main.add_instr(qbe::Instr::Store(qbe::Type::Word, place, val));
-                    },
-                    _ => unreachable!()
-                }
-
-            }
-            ir::Instr::Jump(i) => {
-                main.add_instr(qbe::Instr::Jmp(format!("l_{i}")));
-            }
-        }
-    }
-
     module
-}
-
-fn add_label(func: &mut Function, i: LabelIndex) {
-    func.add_block(format!("l_{i}"));
-}
-
-fn alloc_and_store_value(func: &mut Function, v: ir::Value, temp_index: ValueIndex) -> Value {
-    let place = alloc_value(func, temp_index);
-    match v {
-        ir::Value::Const(i) => {func.add_instr(qbe::Instr::Store(qbe::Type::Word, place.clone(), Value::Const(i))); place},
-        ir::Value::Temp(i) => {
-            let from = Value::Temporary(format!("ptr_{i}"));
-            func.assign_instr(place.clone(), qbe::Type::Word, qbe::Instr::Load(qbe::Type::Word, from));
-            place
-        }
-    }
-}
-
-fn alloc_value(func: &mut Function, temp_index: ValueIndex) -> Value {
-    let place = Value::Temporary(format!("ptr_{temp_index}"));
-    func.assign_instr(place.clone(), qbe::Type::Long, qbe::Instr::Alloc8(8));
-    place
-}
-
-fn load_value(func: &mut Function, value: ir::Value) -> Value {
-    match value {
-        ir::Value::Const(i) => Value::Const(i), 
-        ir::Value::Temp(i) => {
-            let place = Value::Temporary(format!("value_{i}"));
-            let from = Value::Temporary(format!("ptr_{i}"));
-            func.assign_instr(place.clone(), qbe::Type::Word, qbe::Instr::Load(qbe::Type::Word, from));
-            place
-        }
-    }
 }
--- src/ir.rs
+++ src/ir.rs
@@ -1,6 +1,158 @@
 use crate::{lexer, parser, Spanned};
 use std::collections::HashMap;
 
+
+impl Module {
+    pub fn new() -> Self {
+        Self { functions: vec![] }
+    }
+
+    pub fn from_ast(mut self, ast: &[Spanned<parser::Statement>]) -> Result<Self, Error>{
+        use parser::Statement as s;
+        let main = self.add_function("main".to_string());
+        for node in ast {
+            match &node.v {
+                s::Return(i) => {
+                }
+                _ => todo!()
+            }
+        }
+        Ok(self)
+    }
+
+    fn gen_expr(&mut self, f: &mut Function, e: &parser::Expression) -> Result<ValueIndex, Error>{
+        match e {
+            parser::Expression::Integer(i) => {
+                let place = f.alloc_temp();
+                f.add_instr(Instr::Assign { index: place, v: Value::Const(*i) })?;
+                Ok(place)
+            }
+            parser::Expression::Bool(i) => {
+                let place = f.alloc_temp();
+                f.add_instr(Instr::Assign { index: place, v: Value::Const(*i as u64) })?;
+                Ok(place)
+            }
+            parser::Expression::Char(i) => {
+                let place = f.alloc_temp();
+                f.add_instr(Instr::Assign { index: place, v: Value::Const(*i as u64) })?;
+                Ok(place)
+            }
+            parser::Expression::Identifier(i) => {
+                Ok(*f.vars.get(i).unwrap())
+            }
+            parser::Expression::Binary { left, op, right } => {
+                let l = self.gen_expr(f, &left.v)?;
+                let r = self.gen_expr(f, &right.v)?;
+                let place = f.alloc_temp();
+
+                match op {
+                    lexer::Operator::Plus => {
+                        f.a
+
+                    }
+                }
+            }
+        }
+    }
+
+    fn add_function(&mut self, name: String) -> &mut Function {
+        self.functions.push(Function { name, instructions: vec![], max_temps: 0, max_labels: 0, vars: HashMap::new() });
+        self.functions.first_mut().unwrap()
+    }
+}
+
+impl Function {
+    pub fn alloc_temp(&mut self) -> ValueIndex {
+        self.max_temps += 1;
+        self.max_temps - 1
+    }
+    pub fn alloc_label(&mut self) -> LabelIndex {
+        self.max_labels += 1;
+        self.max_labels - 1
+    }
+    pub fn add_instr(&mut self, i: Instr) -> Result<(), Error> {
+        match i {
+            Instr::Assign { index: idx, .. } => {
+                if idx >= self.max_temps {
+                    return Err(Error::InvalidValueIndex { max: self.max_temps, got: idx });
+                }
+            }
+            Instr::Return { value: Value::Temp(idx)} => {
+                if idx >= self.max_temps {
+                    return Err(Error::InvalidValueIndex { max: self.max_temps, got: idx });
+                }
+            }
+            Instr::Label(idx) => {
+                if idx >= self.max_labels {
+                    return Err(Error::InvalidLabel { max: self.max_labels, got: idx });
+                }
+            }
+            Instr::Jump(idx) => {
+                if idx >= self.max_labels {
+                    return Err(Error::InvalidLabel { max: self.max_labels, got: idx });
+                }
+            }
+            Instr::UnaryOp { r: Value::Temp(idx), .. } => {
+                if idx >= self.max_temps {
+                    return Err(Error::InvalidValueIndex { max: self.max_temps, got: idx });
+                }
+            }
+            Instr::BinaryOp { l: Value::Temp(idx_1), r: Value::Temp(idx_2), .. } => {
+                if idx_1 >= self.max_temps {
+                    return Err(Error::InvalidValueIndex { max: self.max_temps, got: idx_1 });
+                }
+                if idx_2 >= self.max_temps {
+                    return Err(Error::InvalidValueIndex { max: self.max_temps, got: idx_2 });
+                }
+            }
+            Instr::BinaryOp { l: Value::Temp(idx_1), .. } => {
+                if idx_1 >= self.max_temps {
+                    return Err(Error::InvalidValueIndex { max: self.max_temps, got: idx_1 });
+                }
+            }
+            Instr::BinaryOp { r: Value::Temp(idx_2), .. } => {
+                if idx_2 >= self.max_temps {
+                    return Err(Error::InvalidValueIndex { max: self.max_temps, got: idx_2 });
+                }
+            }
+            Instr::JumpNotZero { cond: Value::Temp(idx), to, otherwise } => {
+                if idx >= self.max_temps {
+                    return Err(Error::InvalidValueIndex { max: self.max_temps, got: idx });
+                }
+                if to >= self.max_labels {
+                    return Err(Error::InvalidLabel { max: self.max_labels, got: to });
+                }
+                if otherwise >= self.max_labels {
+                    return Err(Error::InvalidLabel { max: self.max_labels, got: otherwise });
+                }
+            }
+            _ => unreachable!()
+        }
+        self.instructions.push(i);
+        Ok(())
+    }
+}
+
+pub enum Error {
+    InvalidValueIndex {
+        max: ValueIndex,
+        got: ValueIndex,
+    },
+    InvalidLabel {
+        max: LabelIndex,
+        got: LabelIndex,
+    }
+}
+
+#[derive(Debug, Clone, Default)]
+pub struct Function {
+    name: String,
+    instructions: Vec<Instr>,
+    vars: Vars,
+    max_labels: usize,
+    max_temps: usize
+}
+
 #[derive(Debug, Clone, Copy)]
 pub enum Value {
     Const(u64),
@@ -24,7 +176,7 @@ pub enum Instr {
     },
     UnaryOp {
         op: lexer::Operator,
-        r: ValueIndex,
+        r: Value,
         into: ValueIndex
     },
     Return {
@@ -33,7 +185,7 @@ pub enum Instr {
     Label(LabelIndex),
     Jump(LabelIndex),
     JumpNotZero { 
-        index: ValueIndex, 
+        cond: Value, 
         to: LabelIndex,
         otherwise: LabelIndex
     },
@@ -41,115 +193,6 @@ pub enum Instr {
 
 pub type Vars = HashMap<String, ValueIndex>;
 
-
-#[derive(Debug, Default)]
-pub struct IREmmiter {
-    ir: Vec<Instr>,
-    vars: Vars,
-    temp_count: usize,
-    label_count: usize,
-}
-
-impl IREmmiter {
-    pub fn new() -> Self {
-        Self::default()
-    }
-    pub fn generate_ir(mut self, module: &[Spanned<parser::Statement>]) -> Vec<Instr> {
-        for s in module {
-            self.generate_statement(&s.v);
-        }
-
-        self.ir
-    }
-
-    fn generate_statement(&mut self, s: &parser::Statement) {
-        match s {
-            parser::Statement::Return(v) => {
-                let e = self.generate_expr(&v.v);
-                self.ir.push(Instr::Return { value: Value::Temp(e) });
-            }
-            parser::Statement::VarAssign { name, t: _, expr } => {
-                let e = self.generate_expr(&expr.v);
-                self.vars.insert(name.to_string(), e);
-            }
-            parser::Statement::If { cond, body } => {
-                let e = self.generate_expr(&cond.v);
-                let body_idx = self.new_label();
-                let over = self.new_label();
-                self.ir.push(Instr::JumpNotZero {
-                    to: body_idx,
-                    otherwise: over,
-                    index: e,
-                });
-                self.ir.push(Instr::Label(body_idx));
-                for i in body {
-                    self.generate_statement(&i.v);
-                }
-                self.ir.push(Instr::Label(over));
-            }
-            parser::Statement::VarReassign { name, expr } => {
-                let new = self.generate_expr(&expr.v);
-                self.vars.insert(name.to_string(), new);
-            }
-            parser::Statement::While { cond, body } => {
-                let header_idx = self.new_label();
-                let body_idx = self.new_label();
-                let over_idx = self.new_label();
-                self.ir.push(Instr::Label(header_idx));
-                let e = self.generate_expr(&cond.v);
-                self.ir.push(Instr::JumpNotZero { index: e, to: body_idx, otherwise: over_idx });
-                self.ir.push(Instr::Label(body_idx));
-                for b in body {
-                    self.generate_statement(&b.v);
-                }
-                self.ir.push(Instr::Jump(header_idx));
-                self.ir.push(Instr::Label(over_idx));
-            }
-        }
-    }
-
-    fn generate_expr(&mut self, e: &parser::Expression) -> ValueIndex {
-        match e {
-            parser::Expression::Integer(i) => {
-                let place = self.new_temp();
-                self.ir.push(Instr::Assign { index: place, v: Value::Const(*i) });
-                place
-            }
-            parser::Expression::Char(i) => {
-                let place = self.new_temp();
-                self.ir.push(Instr::Assign { index: place, v: Value::Const(*i as u64) });
-                place
-            }
-            parser::Expression::Bool(b) => {
-                let place = self.new_temp();
-                self.ir.push(Instr::Assign { index: place, v: Value::Const(*b as u64) });
-                place
-            }
-            parser::Expression::Identifier(name) => {
-                *self.vars.get(name).unwrap()
-            }
-            parser::Expression::Binary { left, op, right } => {
-                let left = self.generate_expr(&left.v);
-                let right = self.generate_expr(&right.v);
-                let place = self.new_temp();
-                self.ir.push(Instr::BinaryOp { op: *op, l: Value::Temp(left), r: Value::Temp(right), into: place });
-                place
-            }
-            parser::Expression::Unary { op, right } => {
-                let right = self.generate_expr(&right.v);
-                let place = self.new_temp();
-                self.ir.push(Instr::UnaryOp { op: *op, r: right, into: place } );
-                place
-            }
-        }
-    }
-
-    fn new_label(&mut self) -> LabelIndex {
-        self.label_count += 1;
-        self.label_count - 1
-    }
-    fn new_temp(&mut self) -> ValueIndex {
-        self.temp_count += 1;
-        self.temp_count - 1
-    }
+pub struct Module {
+    functions: Vec<Function>
 }
