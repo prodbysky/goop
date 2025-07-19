72e340f2eea5ac415ed4703fae653b47816a9299
stinta <kyrbynyt@gmail.com>
2025-07-19 15:46:40 +0300

    write new location tracking primitives

--- src/main.rs
+++ src/main.rs
@@ -100,12 +100,54 @@ fn display_diagnostic_info<T: std::fmt::Debug>(input: &str, input_name: &str, e:
     });
 }
 
-pub type Span = std::ops::Range<usize>;
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct Span {
+    begin: usize,
+    end: usize,
+}
+
+impl Span {
+    pub fn len(&self) -> usize {
+        assert!(self.begin < self.end);
+        self.end - self.begin
+    }
+
+    pub fn new(begin: usize, end: usize) -> Self {
+        assert!(begin <= end);
+        Self { begin, end }
+    }
+}
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Spanned<T> {
-    pub offset: usize,
-    pub len: usize,
-    pub line_beginning: usize,
-    pub v: T,
+    s: Span,
+    v: T,
+}
+
+impl<T> Spanned<T> {
+    pub fn new(v: T, s: Span) -> Self {
+        Self { v, s }
+    }
+
+    pub fn inner(&self) -> &T {
+        &self.v
+    }
+
+    pub fn span(&self) -> &Span {
+        &self.s
+    }
+
+    pub fn begin(&self) -> usize {
+        self.span().begin
+    }
+    pub fn end(&self) -> usize {
+        self.span().end
+    }
+
+    pub fn map<U>(self, f: fn(Spanned<T>) -> U) -> Spanned<U> {
+        Spanned {
+            s: self.s,
+            v: f(self),
+        }
+    }
 }
